7bb60708b727d10eb830069f0837dfd5f1ad1b93
shravnk <brendan.ode4@gmail.com>
2018-06-17 19:15:17 -0400

    Set up index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();

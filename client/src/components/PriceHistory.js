abab890c1d556e91523381593389e7b963327729
shravnk <brendan.ode4@gmail.com>
2018-06-19 15:54:17 -0400

    Price history component displays 5 day history chart using chart.js

--- client/src/components/PriceHistory.js
+++ client/src/components/PriceHistory.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-
+import {Line} from 'react-chartjs-2'
 
 const PriceHistory = ({historyData, display}) => {
 
   if(display) {
     const historyRows = historyData.map(day => <tr><td>{day.date}</td><td>{day.close}</td></tr>)
+    const historyValues = historyData.map(day => day.close)
+    const historyLabels = historyData.map(day => day.date)
+    const chartData = Object.assign({}, {labels: historyLabels, datasets: [{
+        label: "My First dataset",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: historyValues,
+        }]})
     return (
       <div>
-        <table className="table">
-          {historyRows}
-        </table>
+        < Line data={chartData} />
       </div>
     )
   } else {

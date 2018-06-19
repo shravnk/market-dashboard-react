fb720d5d502b305ada54bd97cac7b9a94e5f2fa9
shravnk <brendan.ode4@gmail.com>
2018-06-19 17:50:22 -0400

    Created user model, defined signup actions

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,36 @@
+export const newUser = (values) => {
+  return (dispatch) => {
+    dispatch({type: 'START_SIGNUP'})
+    return fetch(`http://localhost:3000/api/auth/users`, {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(
+          { "user": {
+            "username" : values.username,
+            "password" : values.password
+          }
+        }),
+      })
+      .then(response => {
+        if (response.status !== 201) {
+          response.json()
+          .then(signupResponse => {
+            let signupAttempt = { message: signupResponse };
+            dispatch({
+              type: 'USER_SIGNUP',
+              payload: signupAttempt
+             })
+          })
+        } else {
+          response.json()
+          .then(signupResponse => {
+            let currentUser = Object.assign({}, signupResponse, {is_authenticated: true}, { message: `Succesfully Created an account as ${signupResponse.username}`})
+            dispatch({
+              type: 'USER_SIGNUP',
+              payload: currentUser
+             })
+          })
+        }history.push('/')
+      })
+  }
+}
---
+++ client/src/reducers/user_reducer.js
@@ -0,0 +1,8 @@
+export default (state = {is_authenticated: false}, action) => {
+  switch ( action.type ) {
+    case 'USER_LOGIN':
+
+    default:
+      return state
+  }
+}
